d5f96e8ecfefb1d6e6f3682b05219ee568fc2490
sachin <sachinnarofficials@gmail.com>
2024-12-24 14:34:11 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CsvToArray from "./components/CsvToArray";
+
+function App() {
+  return (
+    <div className="App">
+      <CsvToArray />
+    </div>
+  );
+}
+
+export default App;
