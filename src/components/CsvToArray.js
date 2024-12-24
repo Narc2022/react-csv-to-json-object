217e47fc635c4c7db5ebf94bbad7f2a3f729ff2a
sachin <sachinnarofficials@gmail.com>
2024-12-24 14:45:39 +0530

    add csv drag and drop

--- src/components/CsvToArray.js
+++ src/components/CsvToArray.js
@@ -1,46 +1,52 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
+import { useDropzone } from "react-dropzone";
 
 const CsvToArray = () => {
   const [data, setData] = useState([]);
 
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      Papa.parse(file, {
-        header: true, // Treat the first row as headers
-        skipEmptyLines: true, // Skip empty lines
-        complete: function (result) {
-          setData(result.data);
-          console.log(result.data); // Array of objects
-        },
-        error: function (error) {
-          console.error("Error parsing CSV file:", error);
-        },
-      });
-    }
+  const handleFileUpload = (file) => {
+    Papa.parse(file, {
+      header: true, // Treat the first row as headers
+      skipEmptyLines: true, // Skip empty lines
+      complete: function (result) {
+        setData(result.data);
+        console.log(result.data); // Array of objects
+      },
+      error: function (error) {
+        console.error("Error parsing CSV file:", error);
+      },
+    });
   };
 
   const onSubmit = (data) => {
     console.log("data", data);
   };
 
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: ".csv",
+    onDrop: (acceptedFiles) => {
+      if (acceptedFiles.length > 0) {
+        handleFileUpload(acceptedFiles[0]);
+      }
+    },
+  });
+
   return (
     <div>
       <h2>Upload CSV File</h2>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        {/* <input type="file" accept=".csv" onChange={handleFileUpload} /> */}
-        <button onClick={() => onSubmit(data)}>submit</button>
+        <button onClick={() => onSubmit(data)}>Submit</button>
       </div>
 
-      <div class="flex items-center justify-center w-full">
-        <label
-          for="dropzone-file"
-          class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      <div className="flex items-center justify-center w-full">
+        <div
+          {...getRootProps()}
+          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
         >
-          <div class="flex flex-col items-center justify-center pt-5 pb-6">
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <svg
-              class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+              className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -48,29 +54,22 @@ const CsvToArray = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
               />
             </svg>
-            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span class="font-semibold">Click to upload</span> or drag and
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or drag and
               drop
             </p>
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              SVG, PNG, JPG or GIF (MAX. 800x400px)
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              Only CSV files allowed
             </p>
           </div>
-          {/* <input id="dropzone-file" type="file" class="hidden" /> */}
-          <input
-            type="file"
-            accept=".csv"
-            onChange={handleFileUpload}
-            id="dropzone-file"
-            class="hidden"
-          />
-        </label>
+          <input {...getInputProps()} />
+        </div>
       </div>
     </div>
   );
